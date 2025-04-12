8b984ebb4b52d587a078a281c7489c057842d793
anamsayyed016 <anamsayyed58@gmail.com>
2025-04-12 17:45:59 +0530

    react

--- pakistanicollec/src/Create.jsx
+++ pakistanicollec/src/Create.jsx
@@ -33,17 +33,17 @@ const Create =()=>{
 
           <form action="" onSubmit={Finalsubmit}>
             <label htmlFor="">Your Name</label>
-      <input type="text" name="your_name" placeholder=" First and Last name" onChange={inpchange}/> <br />
+      <input type="text" name="your_name" placeholder=" First and Last name" onChange={inpchange} required/> <br />
 
       <label htmlFor="">Mobile Number</label>
-      <input type="number" name="number" placeholder="Mobile Number" onChange={inpchange} /> <br /> 
+      <input type="tel" name="number" placeholder="Mobile Number" onChange={inpchange} required /> <br /> 
 
       <label htmlFor="">Email</label>
-      <input type="text" name="email" placeholder=" Your Email" onChange={inpchange}/> <br />
+      <input type="text" name="email" placeholder=" Your Email" onChange={inpchange} required/> <br />
 
 
       <label htmlFor="">Password</label>
-      <input type="text" name="password" placeholder="Your Password" onChange={inpchange}/> <br /> 
+      <input type="text" name="password" placeholder="Your Password" onChange={inpchange} required/> <br /> 
 
       <input type="submit" value="Create Your Ade-e-Libaas account" onClick={gotoLogin} />
           </form>
